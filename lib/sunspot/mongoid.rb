beabc20511dd403812d81d95eb129998cb68f58a
Gavin Dunne <g@veen.ca>
2011-01-14 21:10:09 -0800

    added solr_index as class method because original was broken

--- lib/sunspot/mongoid.rb
+++ lib/sunspot/mongoid.rb
@@ -19,10 +19,32 @@ module Sunspot
     def self.included(base)
       base.class_eval do
         extend Sunspot::Rails::Searchable::ActsAsMethods
+        extend Sunspot::Mongoid::ActsAsMethods
         Sunspot::Adapters::DataAccessor.register(DataAccessor, base)
         Sunspot::Adapters::InstanceAdapter.register(InstanceAdapter, base)
       end
     end
+
+    module ActsAsMethods
+      # ClassMethods isn't loaded until searchable is called so we need
+      # call it, then extend our own ClassMethods.
+      def searchable (opt = {}, &block)
+        super 
+        extend ClassMethods
+      end
+    end
+
+    module ClassMethods
+      # The sunspot solr_index method is very dependent on ActiveRecord, so
+      # we'll change it to work more efficiently with Mongoid.
+      def solr_index(opt={})
+        all.each do |m|
+          Sunspot.index(m)
+        end
+        Sunspot.commit
+      end
+    end
+
 
     class InstanceAdapter < Sunspot::Adapters::InstanceAdapter
       def id
